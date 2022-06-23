0d0a7e92778c5242a6fc94e077d2477b2656a844
gavasto <81271153+gavasto@users.noreply.github.com>
2022-06-23 09:44:53 +0700

    render profile data

--- mobile/src/screens/Profile/UserProfile.js
+++ mobile/src/screens/Profile/UserProfile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, Avatar, Card, Colors } from "react-native-ui-lib";
 import {
   SafeAreaView,
@@ -8,7 +8,30 @@ import {
 } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 
+
 const ProfileScreen = () => {
+  const [userData, setUserData] = useState({});
+  const fetchUserInformation = async (userId) => {
+    const token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0NjdkMGJiMC02ODY0LTQ2ODEtYjg5Yi0zMTE4MjAxMmRmNTgiLCJpYXQiOjE2NTU5NTA4MzcsImV4cCI6MTY1NTk1MjYzNywidHlwZSI6ImFjY2VzcyJ9.JByCeeBnv5VmQ5NM8g2RARBBZcXedxe39GmWlO1AiCE";
+    try {
+      let data = await fetch(`http://192.168.2.104:3000/v1/user/${userId}`, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`,
+        },
+      });
+      data = await data.json();
+      setUserData(data);
+      console.log("data", data);
+    } catch (error) {
+      console.error("error", error);
+    }
+  };
+  useEffect(() => {
+    fetchUserInformation("467d0bb0-6864-4681-b89b-31182012df58");
+  }, []);
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.headerContainer}>
@@ -29,17 +52,17 @@ const ProfileScreen = () => {
             size={70}
           />
           <View marginLeft={10}>
-            <Text style={styles.title}>Gavasto02</Text>
+            <Text style={styles.title}>{userData.username}</Text>
             <Text>
               <Icon size={10} name="ellipse" color="blue" />
-              Role
+              {userData.role == 0 ? "Admin" : (userData.role == 1 ? "Moderator" : "User")}
             </Text>
           </View>
         </View>
         <View style={styles.infoSection}>
           <Card style={styles.QA_card}>
             <Text text10 center black>
-              150
+              {userData.numOfQuestions}
             </Text>
             <Text text60 center black>
               Questions
@@ -47,7 +70,7 @@ const ProfileScreen = () => {
           </Card>
           <Card style={styles.QA_card}>
             <Text text10 center black>
-              1500
+            {userData.numOfAnswers}
             </Text>
             <Text text60 center black>
               Answers
@@ -58,11 +81,7 @@ const ProfileScreen = () => {
         <Card style={styles.infoCard}>
           <Text text60>
             <Text black>Full Name:{"  "}</Text>
-            Nguyễn Vũ Kiến Quốc
-          </Text>
-          <Text text60>
-            <Text black>Join date:{"   "}</Text>
-            30/5/2022
+            {userData.name}
           </Text>
         </Card>
         <TouchableOpacity activeOpacity={0.7}>
@@ -74,10 +93,21 @@ const ProfileScreen = () => {
               color: "#1e90ff",
             }}
           >
-            <Icon size={20} name="create-outline" /> Edit Profile
+            <Icon size={20} name="eye" /> My questions
           </Text>
         </TouchableOpacity>
-
+        <TouchableOpacity activeOpacity={0.7}>
+          <Text
+            style={{
+              lineHeight: 50,
+              fontSize: 20,
+              textDecorationLine: "underline",
+              color: "#1e90ff",
+            }}
+          >
+            <Icon size={20} name="create-outline" /> Edit Profile
+          </Text>
+        </TouchableOpacity>            
         <View style={styles.logOutButton}>
           <TouchableOpacity
             style={{ backgroundColor: "red", borderRadius: 20 }}
@@ -138,9 +168,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-around",
   },
   logOutButton: {
-    //flex: 1,
     justifyContent: "flex-end",
-    //paddingBottom: 90,
     alignItems: "flex-end",
   },
   logOutText: {
